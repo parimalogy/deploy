32c4fd8e4662f613e16261be56bc40e12b66e39a
parimalogy <parimallokhande@gmail.com>
2014-09-16 21:46:05 +0100

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,1 +1,38 @@
-Put ui data here  
+library(shiny)
+
+shinyUI(fluidPage(
+    
+#-------------------------------------------
+    titlePanel("Spot relative to Trend"),
+#-------------------------------------------
+    sidebarLayout(
+        sidebarPanel(
+            fileInput('file1', 'Choose CSV File',
+                      accept=c('text/csv', 
+                               'text/comma-separated-values,text/plain', 
+                               '.csv')),
+            tags$hr(),
+            checkboxInput('header', 'Header', TRUE),
+            selectInput('sep', 'Separator',
+                         c(Comma=',',
+                           Semicolon=';',
+                           Tab='\t'),
+                         ',')
+        ),
+
+        mainPanel(
+            tabsetPanel(type = "tabs", 
+                        tabPanel("Ranking Chart", plotOutput("plot")), 
+                        tabPanel("Summary", 
+                                 selectInput("variable", "Select projection type:",
+                                             list("Latest 20days" = "abc", 
+                                                  "40 Days Regime" = "pqr")),
+                                 numericInput('days', 'Number of Days', 3),
+                                 plotOutput("projection"),
+                                 dataTableOutput("summary")),
+                        
+                        tabPanel("SpotData", tableOutput("contents"))
+        )
+    )
+)))
+
